fcd3cb7eaf982fea0f42c75bae80ed44f6cb802a
IT19374666 <68698512+IT19374666@users.noreply.github.com>
2021-09-29 00:56:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/app'));
+app.use(express.static('./dist/FDM_Mini_Front_End'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app/'}),
+    res.sendFile('index.html', {root: 'dist/FDM_Mini_Front_End/'}),
 );
 
 // Start the app by listening on the default Heroku port
